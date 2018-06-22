7f99732d83d3d857204782dc38f333110b094397
mikibouns <mikibouns@gmail.com>
2018-06-22 11:39:30 +0200

    corrected version

--- install_env.py
+++ install_env.py
@@ -1,41 +1,40 @@
+#!/usr/bin/python3
+
 import platform
 from subprocess import call
 import sys
+import os
 
 
 def check_python():
     if float(sys.version[0:3]) >= 3.4:
         return True
-    else:
-        print('!!!you have an old version of the python, you need version 3.4 and higher!!!')
-        return False
+    return False
 
 
 def install_win_env():
     print('Windows')
+    path_activate_env = os.path.join(os.getcwd(), r'venv\Scripts\activate.bat')
     if check_python():
-        try:
-            call(r'python -m venv env && . .\env\Scripts\activate.bat && pip install -r requirements.txt', shell=True)
-        except Exception as e:
-            print(e)
+        call('python -m venv venv && {} && pip install -r requirements.txt'.format(path_activate_env), shell=True)
+        print('created virtual environment and installed all packages')
+        os.ti
 
 
 def install_linux_env():
     print('Linux')
     if check_python():
-        try:
-            call(r'python3 -m venv env && . env/bin/activate && pip install -r requirements.txt', shell=True)
-        except Exception as e:
-            print(e)
-
-
+        call(r'python3 -m venv env && . env/bin/activate && pip install -r requirements.txt', shell=True)
+    else:
+        print('!!!you have an old version of the python, you need version 3.4 and higher!!!')
 
 
 def install_mac_env():
     print('MacOS')
 
 
-if __name__ == '__main__':
+def main():
+
     if platform.system() == 'Windows':
         install_win_env()
     elif platform.system() == 'Linux':
@@ -44,3 +43,7 @@ if __name__ == '__main__':
         install_mac_env()
     else:
         print('!!!platform is not defined!!!')
+
+
+if __name__ == '__main__':
+    main()
