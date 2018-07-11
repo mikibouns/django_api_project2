0a537e4acf92018dc056a18924297bf09caeeea2
mikibouns <mikibouns@gmail.com>
2018-07-11 12:27:19 +0200

    мвамывамыв

--- api_app/utils.py
+++ api_app/utils.py
@@ -3,10 +3,9 @@ from rest_framework.views import exception_handler
 
 def custom_exception_handler(exc, context):
     response = exception_handler(exc, context)
-    print(response)
-    if 'detail' not in response.data:
-        response.data = {'detail': response.data}
     if response is not None:
+        if 'detail' not in response.data:
+            response.data = {'detail': response.data}
         response.data['message'] = response.status_code
         response.data['success'] = 0
         response.data['exception'] = response.data.pop('detail')
